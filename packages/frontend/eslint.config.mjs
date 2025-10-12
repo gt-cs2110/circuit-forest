d5e9ecf54311429a7cd1a4166b794a46e22ab084
cubedhuang <99danielh@gmail.com>
2025-10-12 02:57:11 -0400

    fix eslint

--- packages/frontend/eslint.config.mjs
+++ packages/frontend/eslint.config.mjs
@@ -1,45 +1,65 @@
-import { fixupConfigRules } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginImport from "eslint-plugin-import";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
+import parser from "vue-eslint-parser";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
+export default defineConfig(
+    { ignores: [".vite/"] },
+    {
+        extends: [
+            eslint.configs.recommended,
+            tseslint.configs.eslintRecommended,
+            tseslint.configs.recommended,
+            pluginImport.flatConfigs.recommended,
+            pluginImport.flatConfigs.electron,
+            pluginImport.flatConfigs.typescript,
+            pluginVue.configs["flat/recommended"],
+        ],
 
-export default defineConfig([
-	...pluginVue.configs["flat/recommended"],
-	{
-		extends: fixupConfigRules(
-			compat.extends(
-				"eslint:recommended",
-				"plugin:@typescript-eslint/eslint-recommended",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:import/recommended",
-				"plugin:import/electron",
-				"plugin:import/typescript",
-				"prettier",
-			),
-		),
+        // Apply these rules to these types of files
+        files: ["**/*.{js,ts,tsx,vue}"],
 
-		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.es2020,
+                ...globals.node,
+            },
 
-			parser: tsParser,
-		},
+            parser,
+            ecmaVersion: 6,
+            sourceType: "module",
 
-		ignores: ["node_modules/**", "out/**", ".vite/**"],
-	},
-]);
+            parserOptions: {
+                parser: tseslint.parser,
+            },
+        },
+
+        rules: {
+            // too many false positives
+            "import/no-unresolved": "off",
+            // eslint is correct for this lint, but it's a small webapp who cares
+            "vue/multi-word-component-names": "off",
+            // typescript handles it and it's causing false positives on eslint's side
+            "no-redeclare": "off",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    // ignore any unused variables and arguments starting with _
+                    varsIgnorePattern: "^_",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                },
+            ],
+            // any is bad, but also there's too many APIs with missing types
+            // that would be too annoying to type.
+            // i'll trust that y'all know what y'all're doing if you do :any
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+    eslintConfigPrettier,
+);
